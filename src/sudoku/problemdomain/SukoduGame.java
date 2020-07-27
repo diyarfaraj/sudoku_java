79006add20f8833eadc37830e5a3c07f5c8beba7
Diyar Faraj <diyar.faraj@gmail.com>
2020-07-27 21:34:37 +0200

    git init

---
+++ src/sudoku/problemdomain/SukoduGame.java
@@ -0,0 +1,23 @@
+package sudoku.problemdomain;
+
+import java.io.Serializable;
+
+public class SukoduGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDRY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+}
