c2f2aa42ed50be5362789b8198edba7bdbede814
Diyar Faraj <diyar.faraj@gmail.com>
2020-07-28 21:11:30 +0200

    added the drawing classses

--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -1,31 +1,169 @@
 package sudoku.userinterface;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import sudoku.problemdomain.Coordinates;
+import javafx.scene.text.Font;
 
+import javax.swing.*;
 import java.util.HashMap;
 
 public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHandler<KeyEvent> {
     public final Stage stage;
-    private final Group group;
+    private final Group root;
 
     //keep track of 81 fields
     private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
+    private IUserInterfaceContract.Eventlistener listener;
 
-    private IUserInterfaceContract.Eventlistener listener
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_X = 668;
+    private static final double BOARD_PADDING = 50;
+    private static final double BOARD_X_AND_Y = 576;
+
+    private static final Color WINDOW_BACKGROUND_COLOR= Color.rgb(0, 150, 136);
+    private static final Color BOARD_BACKGROUND_COLOR= Color.rgb(224, 242, 241);
+    private static final String SUDOKU = "Sudoku";
 
 
-    @Override
-    public void handle(KeyEvent keyEvent) {
+
+
+
+    public UserInterfaceImpl(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
+    }
+
+    private void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
+
+    }
+    private void drawGridLines(Group root){
+        int XandY = 114;
+        int index = 0;
+
+        while (index < 8){
+            int thickness;
+            if(index == 2 || index == 5){
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+
+            Rectangle verticalLine = getLine(
+                  XandY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness
+            );
+            Rectangle horizontalLine = getLine(
+                    BOARD_PADDING,
+                    XandY + 64 * index,
+                    thickness,
+                    BOARD_X_AND_Y
+                    );
+
+            root.getChildren().addAll(
+                    verticalLine,
+                    horizontalLine
+            );
+
+            index++;
+        }
+    }
+
+    private Rectangle getLine(double x, double y, double height, double width) {
+
+        Rectangle line = new Rectangle();
+        line.setX(x);
+        line.setY(y);
+        line.setHeight(height);
+        line.setWidth(width);
+
+        line.setFill(Color.BLACK)
+
+        return line;
+    }
+
+    private void drawTextFields(Group root){
+        //where to start drawing the numbers
+        final int xOrigin = 50;
+        final int yOrigin = 50;
+        //how much to move the x or y value after each loop
+        final int xAndYDelta = 64;
+
+
+        for (int xIndex = 0; xIndex < 9; xIndex++) {
+            for (int yIndex = 0; yIndex < 9; yIndex++) {
+                int x = xOrigin + xIndex * xAndYDelta;
+                int y = yOrigin + yIndex * xAndYDelta;
+                //draw it
+                SudokuTextField tile = new SudokuTextField(xIndex, yIndex);
+
+                //encapsulated style information
+                styleSudokuTile(tile, x, y);
+
+                //Note: Note that UserInterfaceImpl implements EventHandler<ActionEvent> in the class declaration.
+                //By passing "this" (which means the current instance of UserInterfaceImpl), when an action occurs,
+                //it will jump straight to "handle(ActionEvent actionEvent)" down below.
+                tile.setOnKeyPressed(this);
+
+                textFieldCoordinates.put(new Coordinates(xIndex, yIndex), tile);
+
+                root.getChildren().add(tile);
+            }
+        }
+    }
+
+    /**
+     * Helper method for styling a sudoku tile number
+     * @param tile
+     * @param x
+     * @param y
+     */
+    private void styleSudokuTile(SudokuTextField tile, double x, double y) {
+        Font numberFont = new Font(32);
+        tile.setFont(numberFont);
+        tile.setAlignment(Pos.CENTER);
+
+        tile.setLayoutX(x);
+        tile.setLayoutY(y);
+        tile.setPrefHeight(64);
+        tile.setPrefWidth(64);
+
+        tile.setBackground(Background.EMPTY);
+    }
+
+
+    private void drawSudokuBoard(Group root){
 
     }
 
+    private void drawTitle(Group root){
+
+    }
+    private void drawBackground(Group root){
+
+    }
+
+
+
     @Override
     public void setListener(IUserInterfaceContract.Eventlistener listener) {
-
+        this.listener = listener
     }
 
     @Override
@@ -47,4 +185,11 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
     public void showError(String message) {
 
     }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
+
+    }
+
+
 }
